cb265484a94f14e8d416e1034e36e435f3a91ffd
michael aiwekhoe <aiwekhoemicheal@gmail.com>
2023-03-23 15:54:48 +0000

    readme

--- dartwork.dart
+++ dartwork.dart
@@ -6,26 +6,28 @@ void main(){
 
   print(formalName('Michael', 'Aiwekhoe'));
 
-  String alphabets = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-  List<String> splitted = alphabets.split(' ');
+  String alphabets = "a b c d"; // string list
+  List<String> splitted = alphabets.split(' '); // split into an array
+  print(splitted.indexOf("d"));
   print(splitted.length);
   print("mark");
   int count = 0;
-  for(int i = 0; i < splitted.length; i++){
-    count += alphabets.indexOf(splitted[i]);
-   
+  for(int i = 0; i < splitted.length; i++){  // iterate through the newly created programing language
+    count += alphabets.indexOf(splitted[i]); // get the index of each item on the array and increment it by count int variable
+    print(splitted[1]);
 
   } 
   print(count);
+  
 
   // String alphabets = "abcdefghijklmnopqrstuvwxyz";
-
+ 
   // print(alphabets.indexOf("c"));
   print(alphabets.indexOf("a"));
   print(nameToNumber("tim"));
   print("mark 2");
 
-}
+} 
 
 String personalGreeting(String name){
   return 'Hey ${'$name!👋'} ';
@@ -50,6 +52,7 @@ int nameToNumber(String name){
   for(int i = 0; i < splitted.length; i++){
     count += alphabets.indexOf(splitted[i]) + 1;
   }
+
 
   return count; // so indexing from zero is overwritten
 }
