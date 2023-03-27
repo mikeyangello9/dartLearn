a99429d96c8c18835868e1d1e1b59d98582ad05d
michael aiwekhoe <aiwekhoemicheal@gmail.com>
2023-03-27 15:56:07 +0100

    froms

--- inclass.dart
+++ inclass.dart
@@ -3,14 +3,27 @@ void main(){
 
   int count =  0;
 
-  for(int i = 1; i < 10; i++){
+
+  for(int i = 1; i < 1000; i++){
     if(i % 3 == 0 || i % 5 == 0){
       count += i;
-      // print(count);
+      
     }
-    // print(count);
   }
   print(count);
+
+  print(fibonacci(1, 2, 10));
+ 
+
+
+}
+
+int fibonacci(int firstTerm, int secondTerm, int nthTimes){
+  int count = 0;
+  for(int i = 0; i < nthTimes; i++){
+    count = firstTerm + secondTerm;
+  }
+  return count;
 }
 
 
